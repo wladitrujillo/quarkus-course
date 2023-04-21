550f4e2a8d3e8bb881e1b3348e92175cc55dddb5
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-04-21 17:38:50 -0500

    feat: add customer entity

---
+++ customer/src/test/java/org/agoncal/quarkus/jpa/CustomerRepositoryTest.java
@@ -0,0 +1,29 @@
+package org.agoncal.quarkus.jpa;
+
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@QuarkusTest
+public class CustomerRepositoryTest {
+    @Inject
+    CustomerRepository repository;
+
+    @Test
+    @TestTransaction
+    public void testCreateAndFindAnArtist() throws SQLException {
+
+        Customer customer = new Customer("firstName", "lastName", "bio");
+        repository.persist(customer);
+        assertNotNull(customer.getId());
+
+        repository.findById(customer.getId());
+        assertEquals("firstName", customer.getFirstName());
+    }
+}
