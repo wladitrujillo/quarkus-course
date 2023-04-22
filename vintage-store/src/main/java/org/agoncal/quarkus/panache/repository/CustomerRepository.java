41d85191df2f42d446afa8e329380e678a468fbf
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2023-04-21 20:29:04 -0500

    feat: manage entities with repository

---
+++ vintage-store/src/main/java/org/agoncal/quarkus/panache/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package org.agoncal.quarkus.panache.repository;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import org.agoncal.quarkus.jdbc.Artist;
+import org.agoncal.quarkus.jpa.Customer;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class CustomerRepository implements PanacheRepository<Customer> {
+}
---
+++ vintage-store/src/test/java/org/agoncal/quarkus/panache/repository/CustomerRepositoryTest.java
@@ -0,0 +1,30 @@
+package org.agoncal.quarkus.panache.repository;
+
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.junit.QuarkusTest;
+import org.agoncal.quarkus.jpa.Customer;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@QuarkusTest
+public class CustomerRepositoryTest {
+    @Inject
+    CustomerRepository repository;
+
+    @Test
+    @TestTransaction
+    public void testCreateAndFindAnArtist() throws SQLException {
+
+        Customer customer = new Customer("firstName", "lastName", "bio");
+        repository.persist(customer);
+        assertNotNull(customer.getId());
+
+        customer = repository.findById(customer.getId());
+        assertEquals("firstName", customer.getFirstName());
+    }
+}
